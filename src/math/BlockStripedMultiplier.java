a26f1a57828346f5e46ec1ca5d460e789a3f8f7e
Данила Трофимов <danya.trofimov@gmail.com>
2023-03-23 18:21:40 +0200

    block-striped multiplier

--- src/math/BlockStripedMultiplier.java
+++ src/math/BlockStripedMultiplier.java
@@ -2,4 +2,16 @@ package math;
 
 public class BlockStripedMultiplier extends Multiplier{
 
+    public BlockStripedMultiplier(int[][] result, int[][] matrixA, int[][] matrixB, int row) {
+        super(result, matrixA, matrixB, row);
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < matrixB[0].length; i++) {
+            for (int j = 0; j < matrixA[0].length; j++) {
+                result[row][j] += matrixA[row][i] * matrixB[j][i];
+            }
+        }
+    }
 }
--- src/math/Multiplier.java
+++ src/math/Multiplier.java
@@ -1,4 +1,16 @@
 package math;
 
 public abstract class Multiplier extends Thread {
+    protected int[][] matrixA;
+    protected int[][] matrixB;
+    protected int[][] result;
+    protected int row;
+    public Multiplier(int[][] result, int[][] matrixA, int[][] matrixB, int row) {
+        if (matrixA[0].length != matrixB.length)
+            throw new IllegalArgumentException("Matrix A's column count must match matrix B's row count.");
+        this.result = result;
+        this.matrixA = matrixA;
+        this.matrixB = matrixB;
+        this.row = row;
+    }
 }
