b8a50974295be6a77b4f70207786f5c0ff833ec0
Данила Трофимов <danya.trofimov@gmail.com>
2023-04-20 15:43:21 +0300

    efficiency test for lab4

--- src/Task1_1/AsynchBankTest.java
+++ src/Task1_1/AsynchBankTest.java
@@ -1,16 +1,34 @@
 package Task1_1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AsynchBankTest {
     public static final int NACCOUNTS = 10;
     public static final int INITIAL_BALANCE = 10000;
+
     public static void main(String[] args) {
-        Bank b = new Bank(NACCOUNTS, INITIAL_BALANCE);
-        int i;
-        for (i = 0; i < NACCOUNTS; i++){
-            TransferThread t = new TransferThread(b, i,
-                    INITIAL_BALANCE);
-            t.setPriority(Thread.NORM_PRIORITY + i % 2);
-            t.start () ;
+        long startTime = System.currentTimeMillis();
+        for (int j = 0; j < 20; j++) {
+            Bank b = new Bank(NACCOUNTS, INITIAL_BALANCE);
+            List<TransferThread> threads = new ArrayList<>();
+            for (int i = 0; i < NACCOUNTS; i++) {
+                TransferThread t = new TransferThread(b, i,
+                        INITIAL_BALANCE);
+                t.setPriority(Thread.NORM_PRIORITY + i % 2);
+                t.start();
+                threads.add(t);
+            }
+            threads.forEach(t -> {
+                try {
+                    t.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
         }
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("Time: " + (endTime - startTime));
     }
 }
