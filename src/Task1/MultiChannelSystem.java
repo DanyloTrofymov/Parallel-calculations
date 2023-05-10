6ef0d2d960fca442a204424f75801a951de7c232
Данила Трофимов <danya.trofimov@gmail.com>
2023-05-10 21:53:54 +0300

    minor refactor

--- src/Task1/MultiChannelSystem.java
+++ src/Task1/MultiChannelSystem.java
@@ -1,7 +1,5 @@
 package Task1;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.*;
 
 import static java.util.concurrent.Executors.newFixedThreadPool;
@@ -16,14 +14,12 @@ public class MultiChannelSystem {
     private static final int ARRIVAL_TIME_MAX = 150;
 
     public static void main(String[] args) {
-        int totalQueuedCustomers = 0;
         int totalFailedCustomers = 0;
         int totalServedCustomers = 0;
         int totalServiceTime = 0;
         int totalQueueLength = 0;
         try (ExecutorService executorService = newFixedThreadPool(SERVICE_CHANNELS)) {
             BlockingQueue<Customer> queue = new ArrayBlockingQueue<>(QUEUE_SIZE);
-            List<Future<Customer>> futures = new ArrayList<>();
 
 
             for (int i = 0; i < TOTAL_CUSTOMERS; i++) {
@@ -37,9 +33,9 @@ public class MultiChannelSystem {
                     totalFailedCustomers++;
                 } else {
                     queue.put(customer);
-                    Future<Customer> future  = executorService.submit(new ServiceChannel(queue, customer));
-                    futures.add(future);
+                    executorService.submit(new ServiceChannel(queue, customer));
                     totalQueuedCustomers++;
+                    totalServiceTime += customer.getServiceTime();
                 }
                 Thread.sleep(getRandomNumberInRange(ARRIVAL_TIME_MIN, ARRIVAL_TIME_MAX));
             }
@@ -47,28 +43,18 @@ public class MultiChannelSystem {
             executorService.shutdown();
             executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.MILLISECONDS);
 
-            for (Future<Customer> future : futures) {
-                try {
-                    totalServiceTime += future.get().getServiceTime();
-                    totalServedCustomers++;
-                } catch (ExecutionException e) {
-                    totalFailedCustomers++;
-                }
-            }
-
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         } finally {
-            printResults(totalQueuedCustomers, totalFailedCustomers, totalServedCustomers, totalServiceTime, totalQueueLength);
+            printResults(totalFailedCustomers, totalServedCustomers, totalServiceTime, totalQueueLength);
         }
 
     }
     private static int getRandomNumberInRange(int min, int max) {
         return (int) (Math.random() * ((max - min) + 1)) + min;
     }
-    private static void printResults(int totalQueuedCustomers, int totalFailedCustomers, int totalServedCustomers, int totalServiceTime, int totalQueueLength) {
+    private static void printResults(int totalFailedCustomers, int totalServedCustomers, int totalServiceTime, int totalQueueLength) {
         System.out.println("Total Customers: " + TOTAL_CUSTOMERS);
-        System.out.println("Total Queued Customers: " + totalQueuedCustomers);
         System.out.println("Total Failed Customers: " + totalFailedCustomers);
         System.out.println("Total Served Customers: " + totalServedCustomers);
         System.out.println("Average Service Time: " + (totalServiceTime / TOTAL_CUSTOMERS));
--- src/Task2/MultiChannelSystem.java
+++ src/Task2/MultiChannelSystem.java
@@ -1,7 +1,5 @@
 package Task2;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.*;
 
 import static java.util.concurrent.Executors.newFixedThreadPool;
