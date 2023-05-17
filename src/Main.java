032ea5ffd078bc7d04c88807ed42fd6cc6500cbc
Данила Трофимов <danya.trofimov@gmail.com>
2023-05-17 19:07:57 +0300

    generator improvement

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,20 @@
 import mathThreads.Multiplier;
 
+import utils.Generator;
+import utils.MatrixType;
 import utils.Reader;
 import utils.Result;
 
 public class Main {
 
-    //        int[][] matrixA = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
-    //        int[][] matrixB = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
     public static void main(String[] args) {
-
-        int[][] matrixA = Reader.readMatrix("1000.txt");
-        int[][] matrixB = Reader.readMatrix("1000.txt");
-            for (int i = 0; i < 7; i++) {
+        int[][] matrixA = Reader.readMatrix("matrixA.txt");
+        int[][] matrixB = Reader.readMatrix("matrixB.txt");
+            for (int i = 0; i < 20; i++) {
                 Result stripedResult = Multiplier.stripedMultiply(matrixA, matrixB, 10);
                 stripedResult.printTime();
             }
-
-
             //Result foxResult = Multiplier.foxMultiply(matrixA, matrixB, matrixA.length / 10, 16);
             //foxResult.printTime();
-
-        //Generator.generateMatrix(10000, 10000, MatrixType.RANDOM, "10000.txt");
-        //Generator.generateMatrix(1000, 1000, MatrixType.RANDOM, "matrixB.txt");
     }
 }
--- src/utils/Generator.java
+++ src/utils/Generator.java
@@ -9,6 +9,7 @@ public class Generator {
     static FileWriter fileWriter;
     static int MAX_RANDOM_NUM = 500;
     static int MIN_RANDOM_NUM = -500;
+
     public static void generateMatrix(int rows, int columns, MatrixType matrixType, int scalar, String fileName) {
         try {
             file = new File(fileName);
@@ -29,17 +30,17 @@ public class Generator {
         generateMatrix(rows, columns, matrixType, 0, fileName);
     }
 
-
-    private static int getValueForMatrixType(int i, int j, MatrixType matrixType, int scalar){
+    private static int getValueForMatrixType(int i, int j, MatrixType matrixType, int scalar) {
         return switch (matrixType) {
             case SCALAR -> i == j ? scalar : 0;
             case UNIT -> i == j ? 1 : 0;
-            case DIAGONAL -> i == j ? getRandomNumber() : 0;
-            case TRIANGLE -> i >= j ? getRandomNumber() : 0;
+            case DIAGONAL -> i == j ? (scalar == 0 ? getRandomNumber() : scalar) : 0;
+            case TRIANGLE -> i >= j ? (scalar == 0 ? getRandomNumber() : scalar) : 0;
             case RANDOM -> getRandomNumber();
             default -> 0;
         };
     }
+
     private static int getRandomNumber() {
         return ThreadLocalRandom.current().nextInt(MIN_RANDOM_NUM, MAX_RANDOM_NUM + 1);
     }
